4724c6d6624e9ff03642982036fe53dbeff78f49
MarinaBarros1327 <marinabarros1327@gmail.com>
2025-02-17 15:50:02 -0300

    meu projeto conta banco

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+       //TODO: Conhecer e importar a classe scenner
+
+
+       //Exibir as mensagem para o nosso usuário
+       
+       
+       //Obter pela scanner os valores digitados no terminal
+
+
+       //Exibir a mensagem da conta criada
+    }
+}
